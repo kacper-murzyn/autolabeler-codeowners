8b6347479b3d5bf59e652e4a8143659e2b53df91
Kacper <89013263+kacper-murzyn@users.noreply.github.com>
2021-12-07 14:00:21 +0100

    Update getLabelsFromOwners.ts

--- src/getLabelsFromOwners.ts
+++ src/getLabelsFromOwners.ts
@@ -9,9 +9,11 @@ export async function getLabelsFromOwners(
   owners: Set<string>
 ): Promise<Set<Label>> {
   const labels: Set<Label> = new Set([])
+  var re = /@DataDog/gi; 
   for (const owner of owners) {
+    var ddOwner = str.replace(re, "team")
     labels.add({
-      name: `${owner}`,
+      name: `${ddOwner}`,
       // From the documentation: https://octokit.github.io/rest.js/#octokit-routes-issues-create-label
       // > The hexadecimal color code for the label, without the leading #
       // randomcolor() returns a color code with a '#' prefix, so we remove it
